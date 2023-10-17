5978841dadf35dd50e7deaa6f1821b36d21a1ce2
Ohodrax <rodrigo.cassiano00@gmail.com>
2023-10-17 17:43:39 -0300

    Aula 17 - Flexbox e Alinhamentos

--- App.js
+++ App.js
@@ -4,14 +4,14 @@ import { View, Text } from 'react-native';
 export default class App extends Component{
   render(){
     return(
-      <View style={{flex: 1, backgroundColor: "#DDD"}}>
+      <View style={{flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+        <View style={{width: 50, height: 50, backgroundColor: 'green'}}></View>
 
-        <View style={{height: 65, backgroundColor: '#222'}}></View>
-
-        <View style={{flex: 1, backgroundColor: '#FFF'}}></View>
+        <View style={{width: 50, height: 50, backgroundColor: 'red'}}></View>
         
-        <View style={{height: 65, backgroundColor: '#222'}}></View>
-
+        <View style={{width: 50, height: 50, backgroundColor: 'yellow'}}></View>
+        
+        <View style={{width: 50, height: 50, backgroundColor: 'blue'}}></View>
       </View>
     );
   }
