b48f99dd77c535535fa429f303ba5048a3109a2c
Ohodrax <rodrigo.cassiano00@gmail.com>
2023-10-18 16:54:30 -0300

    Aula 23 - ScrollView (Vertical e Horizontal)

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 
 export default class App extends Component{
   
   render(){
     return(
       <View style={styles.container}>
-
+        <ScrollView>
+          <View style={styles.box1}></View>
+          <View style={styles.box2}></View>
+          <View style={styles.box3}></View>
+          <View style={styles.box4}></View>
+        </ScrollView>
       </View>
     );
   }
@@ -16,5 +21,21 @@ export default class App extends Component{
 const styles = StyleSheet.create({
   container: {
     flex: 1
-  }
+  },
+  box1:{
+    backgroundColor: 'red',
+    height: 250
+  },
+  box2:{
+    backgroundColor: 'green',
+    height: 250
+  },
+  box3:{
+    backgroundColor: 'yellow',
+    height: 250
+  },
+  box4:{
+    backgroundColor: 'blue',
+    height: 250
+  },
 })
